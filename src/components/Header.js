36aa4348bfad5d5eb28b8af96d87d2e8f275c534
Jenny Steele <jenny@jennyscript.com>
2017-07-06 11:51:02 -0700

    Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
-const Header = () =>
-  <Text>Albums</Text>
+const Header = () => {
+  const { textStyle, viewStyle } = styles
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  )
+}
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+}
 
 export default Header
