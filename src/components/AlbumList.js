d93614ee6e2fa3540d163eb0fb7e6e4056988dd6
Jenny Steele <jenny@jennyscript.com>
2017-07-06 14:06:52 -0700

    Fetch data from the api

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
-class AlbumList extends React.Component {
+class AlbumList extends Component {
+  state = { albums: [] }
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(responseData => this.setState({ albums: responseData }))
+  }
   render() {
+    console.log(this.state.albums)
     return (
       <View>
         <Text>Album List</Text>
